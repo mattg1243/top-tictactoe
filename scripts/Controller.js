e35c4052fa43aa2100978c2a2218261295d56374
mattg1243 <mattgallucci97@gmail.com>
2021-10-26 01:29:36 -0700

    working on checking for a win, have a rough algorithm started

--- scripts/Controller.js
+++ scripts/Controller.js
@@ -5,12 +5,28 @@ let Controller = (function () {
 
     let xPlayer = Player('X');
     let oPlayer = Player('O');
+    let gb = Gameboard.gameboard;
+    let turnsCounter = 0;
 
-
-    // needs to listen for makeMove event 
     xPlayer.isTurn = true;
     
     events.on('moveMade', () => {
+        
+        turnsCounter++;
+        if(turnsCounter > 6) {
+        for (let i = 0; i < 9; i++) {
+            if (gb[i] == gb[i + 3] && gb[i + 3] == gb[i + 6] && gb[i] != '' || 
+                gb[i] == gb[i + 1] && gb[i + 1] == gb[i + 2] && gb[i] != '' ||  
+                gb[i] == gb[i + 4] && gb[i + 4] == gb[i + 8] && gb[i] != '' ||
+                gb[i + 2] == gb[i + 4] && gb[i + 4] == gb[i + 8]) {
+                    events.emit('winner', xPlayer.isTurn ? 'X': 'O')
+                    let winner = gb[i]
+                    alert(`${winner} wins this time!`)
+                    Gameboard.reset();
+                }
+            }
+        }
+
         if (xPlayer.isTurn) {
             xPlayer.isTurn = false;
             oPlayer.isTurn = true;
